641250b2303ff85ee059efdd3ccf2530c8fd508b
leshack <64952843+leshack@users.noreply.github.com>
2020-09-22 20:43:25 +0300

    Add files via upload

---
+++ Trancation error.cpp
@@ -0,0 +1,12 @@
+#include<iostream> 
+using namespace std;
+
+int main(){
+
+long l = 32800;
+short s = (short)l;
+cout<<"The result of u:"<<s<<endl;
+return 0;
+}
+
+
---
+++ signed error.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   short signed i=-32767;
+   short unsigned u=i;
+cout<<"The result of u:"<<u<<endl;
+
+return 0;
+}
